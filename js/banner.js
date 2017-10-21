a17aa939c1b627728a209ae51522bf7a6799e26b
Leonel Castro <leonelrodriguescastroo@gmail.com>
2017-10-21 15:57:13 +0100

    personal web #2

---
+++ js/banner.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+ // Defining a function to set size for #hero 
+	function fullscreen(){
+		jQuery('#jumbotron').css({
+            width: jQuery(window).width(),
+            height: jQuery(window).height()
+        });
+	}
+  
+	fullscreen();
+
+  // Run the function in case of window resize
+  jQuery(window).resize(function() {
+       fullscreen();         
+    });
+
+});
---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+//SCROLL ANIMATION
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
